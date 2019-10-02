e50f8b45bd8ac0c8cd24d69e950df5664627615f
pmorales <pmorales@pmoraless-MBP.fios-router.home>
2019-10-02 18:20:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,73 +1,60 @@
-//console.log("Up and running!");
-
-//var cardOne = "queen";
-//var cardTwo = "king";
-//var cardThree = "queen";
-//var cardFour = "king";
-
-//console.log("User flipped " + cardOne);
 
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
 
+const cardsInPlay = [];
+
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+//function "checkForMatch" validates match of cards
 
 function checkForMatch() {
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again..........................");
-}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
 
 };
 
+//function "flipCard takes in 1 arg"
 
 function flipCard(cardID) {
 
-//var cardOne = cards[0];
-//var cardTwo = cards[2];
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 
+	cardsInPlay.push(cards[cardID].rank);
 
-console.log("User flipped " + cards[cardID]);
-//console.log("User flipped " + cards[cardID]);
-
-//cardsInPlay.push(cardOne);
-cardsInPlay.push(cards[cardID]);
-//cardsInPlay.push(cardTwo);
-
-
-/*
-if (cardsInPlay.length === 2) 
-	console.log ("The Comparison equals to 2 cards at a length of 2 as well!");
-
-if (cardsInPlay[0] === cardsInPlay[2]){
-   alert("you found a match!");
-} else {
-   alert("Sorry, try again!");
-}
-*/
-checkForMatch();
+	checkForMatch();
 
 };
-
-
-
-
 
 flipCard(0);
 flipCard(2);
 
 
-
-
-//testing line 
-//console.log(cardTwo);
-
-
-
-//console.log("User flipped queen - " + cardOne);
-
-//console.log("User flipped king - " + cardTwo);
 
 
