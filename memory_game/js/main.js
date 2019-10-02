ba3ec1b631be90fd38820c565a8b05f9f8a63189
pmorales <pmorales@pmoraless-MBP.fios-router.home>
2019-10-02 13:58:54 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,19 +11,33 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2]
 
-//testing line 
-//console.log(cardTwo);
+function checkForMatch() {
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again..........................");
+}
 
-//console.log("User flipped queen - " + cardOne);
+};
 
-//console.log("User flipped king - " + cardTwo);
 
+function flipCard(cardID) {
+
+//var cardOne = cards[0];
+//var cardTwo = cards[2];
+
+
+console.log("User flipped " + cards[cardID]);
+//console.log("User flipped " + cards[cardID]);
+
+//cardsInPlay.push(cardOne);
+cardsInPlay.push(cards[cardID]);
+//cardsInPlay.push(cardTwo);
+
+
+/*
 if (cardsInPlay.length === 2) 
 	console.log ("The Comparison equals to 2 cards at a length of 2 as well!");
 
@@ -32,3 +46,28 @@ if (cardsInPlay[0] === cardsInPlay[2]){
 } else {
    alert("Sorry, try again!");
 }
+*/
+checkForMatch();
+
+};
+
+
+
+
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+//testing line 
+//console.log(cardTwo);
+
+
+
+//console.log("User flipped queen - " + cardOne);
+
+//console.log("User flipped king - " + cardTwo);
+
+
